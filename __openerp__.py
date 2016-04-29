f258c3019520a5ca012037d1de61b2931b55028b
cristophernic <cristophernic@live.cl>
2016-04-29 20:00:09 -0300

    update openerp

--- __openerp__.py
+++ __openerp__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 {
-    'name': "l10_cl_balance_8 columnas",
+    'name': "l10_cl_balance",
 
     'summary': """
         Tutorial about how to inherit and modify already existing QWeb reports.""",
@@ -15,19 +15,16 @@
     # Categories can be used to filter modules in modules listing
     # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
     # for the full list
-    'category': 'Uncategorized',
+    'category': 'Localization',
     'version': '0.1',
 
     # any module necessary for this one to work correctly
     'depends': ['account'],
-
+    'active': False,
     # always loaded
     'data': [
         # 'security/ir.model.access.csv',
         'templates.xml',
     ],
-    # only loaded in demonstration mode
-    'demo': [
-        'demo.xml',
-    ],
+    'installable': True
 }
