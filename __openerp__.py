e0f2ecbc314b52db21d57d89658802b35376e65e
cristophernic <cristophernic@live.cl>
2016-04-29 19:32:17 -0300

    datos iniciales de modulo

---
+++ __openerp__.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "l10_cl_balance_8 columnas",
+
+    'summary': """
+        Tutorial about how to inherit and modify already existing QWeb reports.""",
+
+    'description': """
+        In this tutorial you will learn how to inherit and modify existing QWeb reports.
+    """,
+
+    'author': "Nueva Gestion",
+    'website': "http://nuevagestion.cl",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/master/openerp/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['account'],
+
+    # always loaded
+    'data': [
+        # 'security/ir.model.access.csv',
+        'templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo.xml',
+    ],
+}
---
+++ models.py
@@ -0,0 +1,87 @@
+# -*- coding: utf-8 -*-
+
+import time
+
+from openerp.osv import osv
+from openerp.report import report_sxw
+from common_report_header import common_report_header
+
+class cl_account_balance(report_sxw.rml_parse, common_report_header):
+    _name = 'report.account.account.balance'
+    _inherit = "account.balance.report"
+    
+    def lines(self, form, ids=None, done=None):
+        def _process_child(accounts, disp_acc, parent):
+                account_rec = [acct for acct in accounts if acct['id']==parent][0]
+                currency_obj = self.pool.get('res.currency')
+                acc_id = self.pool.get('account.account').browse(self.cr, self.uid, account_rec['id'])
+                currency = acc_id.currency_id and acc_id.currency_id or acc_id.company_id.currency_id
+                res = {
+                    'id': account_rec['id'],
+                    'type': account_rec['type'],
+                    'code': account_rec['code'],
+                    'name': account_rec['name'],
+                    'level': account_rec['level'],
+                    'debit': account_rec['debit'],
+                    'credit': account_rec['credit'],
+                    'balance': account_rec['balance'],
+                    'parent_id': account_rec['parent_id'],
+                    'resultado': abs(account_rec['debit'] - account_rec['credit']),
+                    'bal_type': '',
+                }
+                self.sum_debit += account_rec['debit']
+                self.sum_credit += account_rec['credit']
+                if disp_acc == 'movement':
+                    if not currency_obj.is_zero(self.cr, self.uid, currency, res['credit']) or not currency_obj.is_zero(self.cr, self.uid, currency, res['debit']) or not currency_obj.is_zero(self.cr, self.uid, currency, res['balance']):
+                        self.result_acc.append(res)
+                elif disp_acc == 'not_zero':
+                    if not currency_obj.is_zero(self.cr, self.uid, currency, res['balance']):
+                        self.result_acc.append(res)
+                else:
+                    self.result_acc.append(res)
+                if account_rec['child_id']:
+                    for child in account_rec['child_id']:
+                        _process_child(accounts,disp_acc,child)
+
+        obj_account = self.pool.get('account.account')
+        if not ids:
+            ids = self.ids
+        if not ids:
+            return []
+        if not done:
+            done={}
+
+        ctx = self.context.copy()
+
+        ctx['fiscalyear'] = form['fiscalyear_id']
+        if form['filter'] == 'filter_period':
+            ctx['period_from'] = form['period_from']
+            ctx['period_to'] = form['period_to']
+        elif form['filter'] == 'filter_date':
+            ctx['date_from'] = form['date_from']
+            ctx['date_to'] =  form['date_to']
+        ctx['state'] = form['target_move']
+        parents = ids
+        child_ids = obj_account._get_children_and_consol(self.cr, self.uid, ids, ctx)
+        if child_ids:
+            ids = child_ids
+        accounts = obj_account.read(self.cr, self.uid, ids, ['type','code','name','debit','credit','balance','parent_id','level','child_id'], ctx)
+
+        for parent in parents:
+                if parent in done:
+                    continue
+                done[parent] = 1
+                _process_child(accounts,form['display_account'],parent)
+        return self.result_acc
+    
+# class inherit_report_demo(models.Model):
+#     _name = 'inherit_report_demo.inherit_report_demo'
+
+#     name = fields.Char()
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+#     description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         self.value2 = float(self.value) / 100
